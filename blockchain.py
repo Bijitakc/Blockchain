49d8baa4b97fe617aa592586ce0885761b718760
Bijitakc <30403841+Bijitakc@users.noreply.github.com>
2021-10-08 10:25:06 +0545

    Initial commit

---
+++ blockchain.py
@@ -0,0 +1,28 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    
+    #creates a new block and adds it to the chain
+    def new_block(self):
+        pass
+
+    #adds a new tranaction to the list of transactions
+    def new_transaction(self):
+        pass
+
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    #returns last block in chain
+    @property
+    def last_block(self):
+        pass
+
+
+
+
+    
